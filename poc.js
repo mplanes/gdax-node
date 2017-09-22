18f3e0b48d85e322cf35f904da4cab6a9e1a1dd1
Martin Planes <martin.planes@tradivari.com>
2017-09-22 13:39:59 +0200

    bookupdate signal now carries instrument_id

--- poc.js
+++ poc.js
@@ -3,13 +3,14 @@ var orderbookSync = new gdax.OrderbookSync(['BTC-USD', 'ETH-USD']);
 
 console.log(orderbookSync);
 
-orderbookSync.on('bookupdate', function () {
-    book = orderbookSync.books['BTC-USD'].getDepth(10);
-    console.log('BOOK', book);
+orderbookSync.on('bookupdate', function (inst) {
+    console.log('bookupdate', inst);
+    book = orderbookSync.books[inst].getDepth(10);
+    console.log('BOOK', inst, book);
 });
 
-orderbookSync.on('tradeupdate', function (price, size, trade_id) {
-    console.log('TRADE', price, size, trade_id);
+orderbookSync.on('tradeupdate', function (trade) {
+    console.log('TRADE', trade);
 });
 
 

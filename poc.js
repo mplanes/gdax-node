8e5f76744de3605300c20e9e0d8cfd55d7de5ba5
Martin Planes <martin.planes@tradivari.com>
2017-10-14 10:48:47 +0200

    [heartbeat & channels] support for heartbeats and channels all the way

--- poc.js
+++ poc.js
@@ -1,8 +1,8 @@
 var gdax = require('.');
-var orderbookSync = new gdax.OrderbookSync(['BTC-USD', 'ETH-USD']);
+var orderbookSync = new gdax.OrderbookSync(['BTC-USD', 'ETH-USD'], 'https://api.gdax.com', 'wss://ws-feed.gdax.com', null, {heartbeat: false, channels: ['full', 'heartbeat']});
 
 orderbookSync.on('bookupdate', function (inst, time) {
-    console.log('bookupdate', inst, time);
+    //console.log('bookupdate', inst, time);
     //book = orderbookSync.books[inst].getDepth(10);
     //console.log('BOOK', inst, book);
 });
@@ -10,5 +10,10 @@ orderbookSync.on('bookupdate', function (inst, time) {
 orderbookSync.on('tradeupdate', function (trade) {
     console.log('TRADE', trade);
 });
+
+orderbookSync.on('heartbeat', function (instrument, time) {
+    console.log('HB', instrument, time);
+});
+
 
 

23fe0bad9a37e22b386b9d81e22d80c21dfec7e7
Martin Planes <martin.planes@tradivari.com>
2018-02-14 17:39:43 +0100

    [poc] subscribe to all instruments by default

--- poc.js
+++ poc.js
@@ -1,5 +1,5 @@
 var gdax = require('.');
-var orderbookSync = new gdax.OrderbookSync(['BTC-USD', 'ETH-USD'], 'https://api.gdax.com', 'wss://ws-feed.gdax.com', null, {heartbeat: false, channels: ['full', 'heartbeat']});
+var orderbookSync = new gdax.OrderbookSync(['BTC-USD', 'BTC-EUR', 'BTC-GBP', 'LTC-USD', 'LTC-EUR', 'LTC-BTC', 'BCH-USD', 'BCH-EUR', 'BCH-BTC', 'ETH-USD', 'ETH-EUR', 'ETH-BTC'], 'https://api.gdax.com', 'wss://ws-feed.gdax.com', null, {heartbeat: false, channels: ['full', 'heartbeat']});
 
 orderbookSync.on('bookupdate', function (inst, time) {
     //console.log('bookupdate', inst, time);
@@ -13,6 +13,11 @@ orderbookSync.on('tradeupdate', function (trade) {
 
 orderbookSync.on('heartbeat', function (instrument, time) {
     console.log('HB', instrument, time);
+});
+
+
+orderbookSync.on('error', function (error, data) {
+    console.log('ERROR', error, data);
 });
 
 
